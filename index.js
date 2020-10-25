b5f420fe25e506287989864a7e9d33ab593a3198
Martin <m@codedrecordings.com>
2020-10-25 15:10:01 +0100

    WIP swappable color schemes

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-exports.decorateConfig = config => {
-    config.backgroundColor = '#1C1C1C';
-    config.foregroundColor = '#BDBDBD';
-    config.cursorColor = 'rgba(255, 255, 255, 0.75)';
-    config.selectionColor = 'rgba(255, 255, 255, 0.25)';
-    config.showHamburgerMenu = true;
-    config.cursorBlink = true;
-    config.padding = '2px';
-
-    config.colors = {
-        black: '#000000',
+const schemes = {
+    tango: {
+        title: "#2e3436",
+        background: "#181D20",
+        foreground: "#babdb6",
+        black: '#2e3436',
         red: '#cc0000',
         green: '#4e9a06',
         yellow: '#c4a000',
@@ -24,7 +19,43 @@ exports.decorateConfig = config => {
         lightMagenta: '#ad7fa8',
         lightCyan: '#34e2e2',
         lightWhite: '#eeeeec',
-    };
+    },
+    vscode: {
+        title: "#333333",
+        tabs: "#252525",
+        background: "#1e1e1e",
+        foreground: "#BDBDBD",
+        black: "#000000",
+        red: "#cd3131",
+        green: "#0dbc79",
+        yellow: "#e5e510",
+        blue: "#2472c8",
+        magenta: "#bc3fbc",
+        cyan: "#11a8cd",
+        white: "#e5e5e5",
+        lightBlack: "#666666",
+        lightRed: "#f14c4c",
+        lightGreen: "#23d18b",
+        lightYellow: "#f5f543",
+        lightBlue: "#3b8eea",
+        lightMagenta: "#d670d6",
+        lightCyan: "#29b8db",
+        lightWhite: "#f5f5f5",
+    }
+}
+
+exports.decorateConfig = config => {
+    const scheme = schemes.tango;
+
+    config.backgroundColor = scheme.background;
+    config.foregroundColor = scheme.foreground;
+    config.borderColor = scheme.title;
+    config.cursorColor = 'rgba(255, 255, 255, 0.75)';
+    config.selectionColor = 'rgba(255, 255, 255, 0.25)';
+    config.showHamburgerMenu = true;
+    config.cursorBlink = true;
+    config.padding = '2px';
+    config.colors = scheme;
 
     config.css = `
         svg {
@@ -35,7 +66,7 @@ exports.decorateConfig = config => {
             background: ${config.backgroundColor};
         }
         .header_windowHeader {
-            background: ${config.borderColor || '#2C2C2C'};
+            background: ${config.borderColor};
             top: 0;
             left: 0;
             right: 0;
@@ -48,10 +79,12 @@ exports.decorateConfig = config => {
         }
         .tab_tab {
             padding: 0;
-            border-width: 0 0 1px;
+            border: 0 !important;
+            background: ${scheme.tabs || config.borderColor};
         }
         .tab_tab.tab_active {
-            border-bottom-color: #FAFAFA !important;
+            border-top: 1px solid ${scheme.blue} !important;
+            background-color: ${config.backgroundColor};
         }
         .tab_tab .tab_icon {
             opacity: 0.25;
